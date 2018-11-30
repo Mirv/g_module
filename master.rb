62f81ccc1c76bce565c9cc37240c40542b47d0de
Mirv <reski.raptor@gmail.com>
2018-11-30 22:02:16 +0000

    added interface opt for template, company name & touched Templates.JSON

--- master.rb
+++ master.rb
@@ -2,20 +2,20 @@
 
 require 'optparse'
 
+puts "Entering Greeting Script successfully ..."
+
 options = {}
 OptionParser.new do |opt|
-  opt.on('--first_name FIRSTNAME') { |o| options[:first_name] = o }
-  opt.on('--last_name LASTNAME') { |o| options[:last_name] = o }
+  opt.on('--f', '--first_name FIRSTNAME') { |o| options[:first_name] = o }
+  opt.on('--l', '--last_name LASTNAME') { |o| options[:last_name] = o }
+  opt.on('--c', '--company_name LASTNAME') { |o| options[:company_name] = o }
+  opt.on('--t', '--template_name LASTNAME') { |o| options[:template_name] = o }
 end.parse!
 
 puts options
 
 Dir.chdir('lib') do
   # setup our dependencies
-  load 'load_guest.rb'
-  
-  # def initialize
-    puts "Hi"
-    puts "--#{LoadGuest.load('Guests.json', 'data')}"
-  # end
+  target_file = 'load_guest.rb'
+  puts "Loading ... '#{target_file}' -- loaded successfully: #{load 'load_guest.rb'}"
 end
