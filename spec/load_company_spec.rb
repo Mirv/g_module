986a28c47db4f37b3f629d145c87465d137efbb0
Mirv <reski.raptor@gmail.com>
2018-12-13 23:08:28 +0000

    company tests smoothed

--- spec/load_company_spec.rb
+++ spec/load_company_spec.rb
@@ -3,30 +3,36 @@ load 'load_company.rb'
 
 describe "#initialize" do
   context "call to LoadCompany class" do
-    let (:company) { 
-      company = LoadCompany.new(
+    let (:get_company) { LoadCompany.new(
       firstName: "Candy", lastName: "Pace", template: "Default", 
-      company: "The Prancing Pony", directory: "lib/data") 
+      company: "The Prancing Pony", directory: "lib/data") }
+      
+    subject(:company) {
+      get_company.execute_process
+      get_company.data
     }
       
     it "should contain key for company in data when done" do
-      company.execute_process
-      expect(company.data).to have_key('company')
+      expect(company).to have_key('company')
     end
     
     it "should contain key for timezon in data when done" do
-      company.execute_process
-      expect(company.data).to have_key('timezone')
+      # company.execute_process
+      expect(company).to have_key('timezone')
     end
     
     it "should contain value for company in data when done" do
-      company.execute_process
-      expect(company.data['company']).to_not be_empty
+      # company.execute_process
+      expect(company['company']).to_not be_empty
+    end
+    
+    it "should have valid startTimestamp" do
+      expect(company['company']).to be_a(String)
     end
     
     it "should contain key for timezon in data when done" do
-      company.execute_process
-      expect(company.data['timezone']).to_not be_empty
+      # company.execute_process
+      expect(company['timezone']).to_not be_empty
     end
   end
 end
