a8f6ca3d1926d815426d6fba27c7530dda9908ef
Mirv <reski.raptor@gmail.com>
2018-12-13 23:00:25 +0000

    smoothed out tempalte loader

--- spec/load_template_spec.rb
+++ spec/load_template_spec.rb
@@ -4,25 +4,26 @@ require 'json_requires'     # json setup & testing data
 
 describe ".load" do
   context "loaded template" do
-    let (:template) { 
-      template = LoadTemplate.new(
-        first: "Candy", last: "Pace", template: "Default", 
-        company: "The Prancing Pony", directory: "lib/data")
+    let(:template_loader) { loader = LoadTemplate.new(
+      first: "Candy", last: "Pace", template: "Default", 
+      company: "Hotel California", directory: "lib/data")
+    }
+    
+    let (:template_test) { 
+      template_loader.execute_process
+      template_loader.data
     }
 
     it "should have record_lookup return hash with key template" do
-      template.execute_process
-      expect(template.data).to have_key("template")
+      expect(template_test).to have_key("template")
     end
     
     it "should be a string" do
-      template.execute_process
-      expect(template.data['template']).to be_a(String)
+      expect(template_test['template']).to be_a(String)
     end  
     
     it "should have record_lookup return hash with key template" do
-      template.execute_process
-      expect(template.data['template']).not_to be_empty
+      expect(template_test['template']).not_to be_empty
     end
   end
 end
