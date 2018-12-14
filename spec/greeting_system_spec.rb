29a6718ff586c7d953a5f3eb3ef53e76b3c831b8
Mirv <reski.raptor@gmail.com>
2018-12-14 23:31:59 +0000

    switched to custom greetingsystem.new

--- spec/greeting_system_spec.rb
+++ spec/greeting_system_spec.rb
@@ -2,7 +2,6 @@ load 'greeting_system.rb'
 
 describe "#intialization" do
   context "class object inputs" do
-    
     let(:greeting) { GreetingSystem.new(valid_inputs) }
 
     it "should have successfully initialized" do
@@ -10,28 +9,22 @@ describe "#intialization" do
     end
     
     it "test should fail to make new object if first null" do
-      expect{ GreetingSystem.new((valid_inputs.merge!(firstName: ""))) 
+      expect{ greet((valid_inputs.merge!(firstName: ""))) 
       }.to raise_error(ArgumentError, "First Name was empty")
     end
     
-    it "should fail to make new object if first null" do
-      expect{ GreetingSystem.new(
-        firstName: "", lastName: "l", company: "c", template: "d")
-      }.to raise_error(ArgumentError, "First Name was empty")
-    end
-
     it "should fail to make new object if last null" do
-      expect{ GreetingSystem.new((valid_inputs.merge!(lastName: ""))) 
+      expect{ greet((valid_inputs.merge!(lastName: ""))) 
       }.to raise_error(ArgumentError, "Last Name was empty")
     end
 
     it "should fail to make new object if company null" do
-      expect{ GreetingSystem.new((valid_inputs.merge!(company: "")))
+      expect{ greet((valid_inputs.merge!(company: "")))
       }.to raise_error(ArgumentError, "Company Name was empty")
     end
 
     it "should fail to make new object if tempalte null" do
-      expect{ GreetingSystem.new((valid_inputs.merge!(template: "")))
+      expect{ greet((valid_inputs.merge!(template: "")))
       }.to raise_error(ArgumentError, "Template Name was empty")
     end
   end
@@ -43,3 +36,8 @@ def valid_inputs
     company: "The Grand Budapest Hotel", directory: "lib/data"
   }
 end
+
+def greet(hash)
+  GreetingSystem.new(hash)
+end
+  
