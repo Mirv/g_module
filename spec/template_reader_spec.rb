6819eba1c1f3f5995fbd737b33c81f44f869bc87
Mirv <reski.raptor@gmail.com>
2018-12-17 14:28:30 +0000

    cleanup of new class

--- spec/template_reader_spec.rb
+++ spec/template_reader_spec.rb
@@ -20,7 +20,6 @@ describe "TemplateReader finishes" do
   end
   
   it "should add non-nil values to data hash" do
-    puts template.inspect
     expect(result[:template_variables].first).to_not be_nil
   end
   
@@ -28,12 +27,8 @@ end
 
 def valid_template_inputs
   {
-    firstName: "Candy", lastName: "Pace", template: "Default", 
-    company: "The Grand Budapest Hotel", directory: "lib/data",
-    "roomNumber"=>529, "startTimestamp"=>1486654792, "endTimestamp"=>1486852373, 
     :raw_template=>"{timeGreeting} {firstName} {lastName}. Room number {roomNumber} is now available for your use at {company} in {city}.  If you require anything please reach out to us.", 
     :deliminator=>{"start"=>"{", "stop"=>"}"}, 
-    "id"=>1, "company"=>"Hotel California", "city"=>"Santa Barbara", "timezone"=>"US/Pacific"
   }
 end
 
