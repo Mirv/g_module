6f9429d806ce9577f494720d3c47dd4066d1ed94
Mirv <reski.raptor@gmail.com>
2019-01-15 22:46:23 +0000

    loading greeting directory too

--- lib/main.rb
+++ lib/main.rb
@@ -2,8 +2,9 @@ require_relative 'directory_loader'
 # Dir["*.rb"].map{|x| load x}
 # require
 
+# working
 DirectoryLoader::Require.load_cur_dir
-
-
+# working
+DirectoryLoader::Require.load_dir("greet")
 
 
