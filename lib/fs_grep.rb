61d5f3fb75a579274997095614a95eba8c37442a
Mirv <reski.raptor@gmail.com>
2019-01-28 04:44:44 +0000

    revised to allow for no recursion and corrected 2 bugs

--- lib/fs_grep.rb
+++ lib/fs_grep.rb
@@ -7,29 +7,37 @@ class Searcher
   attr_reader :matched
 
   def self.grep(options)
-  # (
-  #   matchString:    matches, 
-  #   pathName:       directory = "", 
-  #   excludedDir:    exclude_dirs = "", 
-  #   fileTypes:      fileTypes = "", 
-  #   recursive_off:  recursive = true
-  #   )
-    new(options[:matchString], "", ".c9, .git, proofs")
+    matches =         options[:matchString]   || ""
+    directory =       options[:pathName]      || ""
+    exclude_dirs =    options[:excludedDir]   || ".c9, .git, proofs" 
+   fileTypes =        options[:fileTypes]     || "rb"
+    recursive_off =   options[:recursive_off] || false
+
+    new(matches, directory, exclude_dirs, fileTypes, recursive_off)
   end
   
   # '' is search current dir
-  def initialize(matches, directory = "", exclude_dirs = "", types = "", recursive_off = false)
+  def initialize(matches, directory = "*", exclude_dirs = "", types = "", recursive_off = false)
     @matches = matches
     @directory = directory.strip
     @types = joiner(include_file_types(types)) || ""
     @exclude_dirs = joiner(excluder(exclude_dirs)) || ""
     @recursive = recursive_off
     @matched = run_matches
-    # puts self.inspect
+    puts self.inspect
   end
   
   def run_matches
-    search = ["grep", "-R", @matches, @exclude_dirs, @directory].join(" ")
+    # Note - recursive  not included
+    basics_params = ["grep", "'#{@matches}'", @types, @exclude_dirs, @directory]
+    if @recursive_off
+      # TODO - hangs without good path? YAGNI? ... no -R is only real diff
+      search = basics_params.join(" ")
+    else
+      basics_params << "-R"
+      search = basics_params.join(" ")
+    end
+    puts search
     system(*search)
   end
   
@@ -55,7 +63,7 @@ class Searcher
     options = {}
     OptionParser.new do |opt|
       opt.on('--m', '--match_string MATCH_STRING') { |o| options[:matchString] = o }
-      opt.on('--c', '--path_name START_PATH_NAME') { |o| options[:pathName] = o }
+      opt.on('--p', '--path_name START_PATH_NAME') { |o| options[:pathName] = o }
       opt.on('--exc_d', '--excluded_dir EXCLUDED_DIR_LIST') { |o| options[:excludedDir] = o }
       opt.on('--inc_f', '--include_types FILE_TYPES_INCLUDED') { |o| options[:fileTypes] = o }
       opt.on('--r_off', '--recursive_off') { |o| options[:recursive_off] = o }
